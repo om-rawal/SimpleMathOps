6d4e7c97187e165977d39357f35f81227342795f
Om <om.rawal@somaiya.edu>
2022-07-05 18:01:42 +0530

    Fixed JUnit Version

--- src/main/java/com/simpleMathOps/OperationTest.java
+++ src/main/java/com/simpleMathOps/OperationTest.java
@@ -1,5 +1,5 @@
 package com.simpleMathOps;
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 import org.junit.Test;
 
@@ -8,29 +8,29 @@ public class OperationTest {
 	@Test
     public void addTest()
     {
-        assertEquals( 30,o1.add(10,20));
+		Assert.assertEquals( 30,o1.add(10,20));
     }
     @Test
     public void subtractTest()
     {
-        assertEquals( -10,o1.subtract(10,20));
+    	Assert.assertEquals( -10,o1.subtract(10,20));
     }
     @Test
     public void divisionTest()
     {
-        assertEquals( 2,o1.getDivision(20,10));
+    	Assert.assertEquals( 2,o1.getDivision(20,10));
     }
     
     @Test
     public void remainderTest()
     {
-        assertEquals( 10,o1.getRemainder(10,20));
+    	Assert.assertEquals( 10,o1.getRemainder(10,20));
     }
     
     @Test
     public void identityTest()
     {	
     	String result=o1.identify();
-        assertEquals( "This is created by Om Rawal",result);
+    	Assert.assertEquals( "This is created by Om Rawal",result);
     }
 }
